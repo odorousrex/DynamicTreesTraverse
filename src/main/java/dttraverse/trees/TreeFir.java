cee44c2277014c875ed2aa654f74890c2eb9b9c2
ferreusveritas <ferreusveritas@gmail.com>
2018-12-05 22:52:39 -0500

    Isolate all conifer growth logic

--- src/main/java/dttraverse/trees/TreeFir.java
+++ src/main/java/dttraverse/trees/TreeFir.java
@@ -1,6 +1,7 @@
 package dttraverse.trees;
 
-import com.ferreusveritas.dynamictrees.systems.GrowSignal;
+import com.ferreusveritas.dynamictrees.ModTrees;
+import com.ferreusveritas.dynamictrees.api.TreeRegistry;
 import com.ferreusveritas.dynamictrees.systems.featuregen.FeatureGenConiferTopper;
 import com.ferreusveritas.dynamictrees.trees.Species;
 import com.ferreusveritas.dynamictrees.trees.TreeFamily;
@@ -12,10 +13,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -30,7 +28,8 @@ public class TreeFir extends TreeFamily {
 			super(treeFamily.getName(), treeFamily, ModContent.firLeavesProperties);
 			
 			setBasicGrowingParameters(0.3f, 16.0f, 3, 3, 0.9f);
-						
+			setGrowthLogicKit(TreeRegistry.findGrowthLogicKit(ModTrees.CONIFER));
+			
 			envFactor(BiomeDictionary.Type.HOT, 0.50f);
 			envFactor(BiomeDictionary.Type.DRY, 0.25f);
 			envFactor(BiomeDictionary.Type.WET, 0.75f);
@@ -45,38 +44,6 @@ public class TreeFir extends TreeFamily {
 		@Override
 		public boolean isBiomePerfect(Biome biome) {
 			return BiomeDictionary.hasType(biome, BiomeDictionary.Type.CONIFEROUS);
-		}
-		
-		@Override
-		protected int[] customDirectionManipulation(World world, BlockPos pos, int radius, GrowSignal signal, int probMap[]) {
-			
-			EnumFacing originDir = signal.dir.getOpposite();
-			
-			//Alter probability map for direction change
-			probMap[0] = 0;//Down is always disallowed for spruce
-			probMap[1] = signal.isInTrunk() ? getUpProbability(): 0;
-			probMap[2] = probMap[3] = probMap[4] = probMap[5] = //Only allow turns when we aren't in the trunk(or the branch is not a twig and step is odd)
-					!signal.isInTrunk() || (signal.isInTrunk() && signal.numSteps % 2 == 1 && radius > 1) ? 2 : 0;
-			probMap[originDir.ordinal()] = 0;//Disable the direction we came from
-			probMap[signal.dir.ordinal()] += signal.isInTrunk() ? 0 : signal.numTurns == 1 ? 2 : 1;//Favor current travel direction
-			
-			return probMap;
-		}
-		
-		@Override
-		protected EnumFacing newDirectionSelected(EnumFacing newDir, GrowSignal signal) {
-			if(signal.isInTrunk() && newDir != EnumFacing.UP){//Turned out of trunk
-				signal.energy /= 3.0f;
-			}
-			return newDir;
-		}
-		
-		@Override
-		public float getEnergy(World world, BlockPos pos) {
-			long day = world.getTotalWorldTime() / 24000L;
-			int month = (int)day / 30;//Change the hashs every in-game month
-			
-			return super.getEnergy(world, pos) * biomeSuitability(world, pos) + (coordHashCode(pos.up(month)) % 5);//Vary the height energy by a psuedorandom hash function
 		}
 		
 	}
